0a979332ef5d5ec10c0ad09b00331fba77e2f201
shahinyanm <shahinyanm@gmail.com>
2019-04-02 17:36:21 +0400

    Generate Question model via artisan model generator

---
+++ database/migrations/2019_04_02_132124_create_questions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->unsignedBigInteger('views')->default(0);
+            $table->unsignedBigInteger('answers')->default(0);
+            $table->integer('votes')->default(0);
+            $table->unsignedBigInteger('best_answer_id')->nullable();
+            $table->bigInteger('user_id')->unsigned()->index()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
