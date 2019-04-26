41c15a3f891cb5000afff5231d36e4352ef794df
shahinyanm <shahinyanm@gmail.com>
2019-04-26 15:08:09 +0400

    Add Votes, Answer & Views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    protected $fillable =['title','body'];
+    protected $fillable = ['title', 'body'];
 
     public function user()
     {
@@ -19,11 +19,26 @@ class Question extends Model
         $this->attributes['slug'] = str_slug($value);
     }
 
-    public function getUrlAttribute(){
+    public function getUrlAttribute()
+    {
         return route('questions.show', $this->id);
     }
 
-    public function getCreatedDateAttribute(){
+    public function getCreatedDateAttribute()
+    {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer) {
+                return 'answered-accepted';
+            }
+            return 'answered';
+        } else {
+
+            return 'unanswered';
+        }
     }
 }
